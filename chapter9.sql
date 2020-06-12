882467eeef2027b5ad9f4bbe2da89cf557b4e3d2
koharakouki <ko.kohara02@gmail.com>
2020-06-13 02:18:20 +0900

    [Fin]chapter9

--- chapter9.sql
+++ chapter9.sql
@@ -1,12 +1,24 @@
 -- 明示的なロック
-BEGIN;                      --トランザクションの始まり
+BEGIN;                      -- トランザクションの始まり
 SELECT * FROM 家計簿
  WHERE 日付 >= '2018-02-01'
-   FOR UPDATE;              --2月以降のデータを明示的ロック
+   FOR UPDATE;              -- 2月以降のデータを明示的ロック
 -- 集計処理1
 SELECT ~ ;
 -- 集計処理2
 SELECT ~ ;
 -- 集計処理3
 SELECT ~ ;
-COMMIT;                     --ロック解除される
+COMMIT;                     -- ロック解除される
+
+
+-- 家計簿テーブルをロックして集計する
+BEGIN;
+LOOK TABLE 家計簿 IN EXCLUSIVE MODE;  -- 表を明示的ロック
+-- 集計処理1
+SELECT ~ ;
+-- 集計処理2
+SELECT ~ ;
+-- 集計処理3
+SELECT ~ ;
+COMMIT;                              -- ロック解除される
