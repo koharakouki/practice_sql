ad9b74224e1fe47e792fc99019514662936fafef
koharakouki <ko.kohara02@gmail.com>
2020-06-14 04:27:43 +0900

    [Add] 題材A レベル6

--- subject_a.sql
+++ subject_a.sql
@@ -346,3 +346,8 @@ SELECT SUBSTRING(名義, 1, 1) AS 名義, COUNT(名義) AS データ件数, AVG(
  GROUP BY SUBSTRING(名義, 1, 1)
 HAVING COUNT(名義) >= 10
     OR AVG(LENGTH(REPLACE(名義, '  ', '')) > 5
+
+
+
+-- LEVEL6
+-- 59
