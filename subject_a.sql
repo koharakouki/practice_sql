a9d89227d409305ffc9bc0e02818ad533b7b1c0f
koharakouki <ko.kohara02@gmail.com>
2020-06-13 22:38:43 +0900

    [Fin]題材A レベル4

--- subject_a.sql
+++ subject_a.sql
@@ -191,3 +191,109 @@ SELECT 口座番号, 名義, '◯' AS 口座区分
 SELECT 口座番号, 名義, '×' AS 口座区分
   FROM 廃止口座
  ORDER BY 名義
+
+
+
+-- LEVEL4
+-- 34
+SELECT 口座番号, 残高/1000 AS 千円単位の残高
+  FROM 口座
+ WHERE 残高 >= 1000000
+
+-- 35
+INSERT INTO 口座
+VALUES ('0652281', 'たかぎ のぶお', '1', 100000 + 3000, '2018-04-01');
+
+INSERT INTO 口座
+VALUES ('1026413', 'まつもと さわこ', '1', 300000 + 3000, '2018-04-02');
+
+INSERT INTO 口座
+VALUES ('2239710', 'ささき しげのり', '1', 100000 + 3000, '2018-04-03');
+
+-- 36
+UPDATE 口座
+   SET 残高 = (残高 - 3000) * 1.003
+ WHERE 口座番号 IN ('0652281 ', '1026413', '2239710')
+
+-- 37
+SELECT 口座番号, 更新日, 更新日 + 180 AS 通帳期限日
+  FROM 口座
+ WHERE 更新日 < '2017-01-01'
+
+-- 38
+SELECT 口座番号, CONCAT((カ,名義)
+  FROM 口座
+ WHERE 種別 = '3'
+
+-- 39
+SELECT DISTINCT 種別 AS 種別コード,
+  CASE 種別
+  WHEN '1' THEN '普通'
+  WHEN '2' THEN '当座'
+  WHEN '3' THEN '別段'
+  END AS 種別名
+ FROM 口座
+
+-- 40
+SELECT 口座番号, 名義,
+  CASE WHEN 残高 < 100000 THEN 'C'
+       WHEN 残高 >= 100000 AND 残高 < 1000000 THEN 'B'
+       ELSE 'A'
+  END AS 残高ランク
+ FROM 口座
+
+-- 41
+SELECT LENGTH(口座番号), LENGTH(REPRACE(名義, '  ', '')), LENGTH(CAST(残高, AS VARCHAR))
+  FROM 口座
+
+-- 42
+SELECT *
+  FROM 口座
+ WHERE SUBSTRING(名義, 1, 5) LIKE '%カワ%'
+
+-- 43
+SELECT *
+  FROM 口座
+ WHERE LENGTH(CAST(残高, AS VARCHAR)) >= 4
+   AND SUBSTRING(LENGTH(CAST(残高, AS VARCHAR)), LENGTH(CAST(残高, AS VARCHAR))-2, 3) = '000'
+
+-- 44
+SELECT 口座番号, 残高, TRUNK(残高 * 0.0002, 0) AS 利息
+  FROM 口座
+ ORDER BY 残高 DESC
+
+-- 45
+SELECT 口座番号, 残高,
+  CASE WHEN 残高 < 500000 THEN TRUNK(残高 * 0.0001, 0)
+       WHEN 残高 >= 500000 AND 残高 < 2000000 THEN TRUNK(残高 * 0.0002, 0)
+       WHEN 残高 >= 2000000 THEN TRUNK(残高 * 0.0003, 0)
+  END AS 残高別利息
+ FROM 口座
+ ORDER BY 残高別利息 DESC, 口座番号
+
+-- 46
+INSERT INTO 口座
+VALUES ('0351262', 'いとかわ　だい', '2', 635110, CURRENT_DATE);
+
+INSERT INTO
+VALUES ('105513', 'あきつ　じゅんじ', '1', 88463, CURRENT_DATE);
+
+INSERT INTO 口座
+VALUES ('1739298', 'ほしの　さとみ', '1', 704610, CURRENT_DATE);
+
+-- 47
+SELECT 口座番号, 名義, 種別, 残高, SUBSTRING(CAST(更新日 AS VARCHAR), 1, 4) || '年' ||
+　　　　　　　　　　　　　　　　　　　SUBSTRING(CAST(更新日 AS VARCHAR), 6, 2) || '月' ||
+                               SUBSTRING(CAST(更新日 AS VARCHAR), 9, 2) || '日' AS 更新日
+  FROM 口座
+ WHERE 更新日 >= '2018-01-01'
+
+-- 48
+SELECT COALESCE(CAST(更新日 AS VARCHAR), '設定なし') AS 更新日
+  FROM 口座
+
+
+
+-- LEVEL5
+-- 49
+
