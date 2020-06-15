b0a0fe619e2071119e27698c5b7b60b1828f9d5b
koharakouki <ko.kohara02@gmail.com>
2020-06-15 20:24:12 +0900

    [Fin]題材B レベル4

--- subject_b.sql
+++ subject_b.sql
@@ -208,4 +208,83 @@ SELECT 商品コード, 商品名, 単価
 
 -- LEVEL4
 -- 34
-BY 1 DESC
+SELECT 商品コード, 単価, TRUNK(単価*0.95, 0) AS キャンペーン価格
+  FROM 商品
+ WHERE 商品区分 = '9'
+ ORDER BY 商品コード
+
+-- 35
+UPDATE 注文
+   SET クーポン割引料 = クーポン割引料 + 300
+ WHERE 注文日 >= '2018-03-12'
+   AND 注文日 < '2018-03-15'
+   AND 数量 >= 2
+   AND クーポン割引料 IN NOT NULL
+
+-- 36
+UPDATE 注文
+   SET 数量 = 数量 - 1
+ WHERE 注文番号 = '201802250126'
+   AND 商品コード = 'W0156'
+
+-- 37
+SELECT CAST(注文番号 AS VARCHAR) || '-' || CAST(注文枝番 AS VARCHAR) AS 番号
+  FROM 注文
+ WHERE 注文番号 >= '201710010001'
+   AND 注文番号 <= '201710319999'
+
+-- 38
+SELECT DISTINCT 商品区分 AS 区分,
+       CASE 商品区分
+       WHEN '1' THEN '衣類'
+       WHEN '2' THEN '靴'
+       WHEN '3' THEN '雑貨'
+       WHEN '9' THEN '未分類'
+       END AS 区分名
+  FROM 商品
+
+-- 39
+SELECT 商品コード, 商品名, 単価,
+       CASE WHEN 単価 < 3000 THEN 'S'
+            WHEN 単価 >= 3000 AND 単価 < 10000 THEN 'M'
+            WHEN 単価 >= 10000 THEN 'L'
+       END AS 販売価格ランク,
+       商品区分 || ':' || CASE 商品区分
+                        WHEN '1' THEN '衣類'
+                        WHEN '2' THEN '靴'
+                        WHEN '3' THEN '雑貨'
+                        WHEN '9' THEN '未分類'
+                        END AS 商品区分
+  FROM 商品
+ ORDER BY 単価, 商品コード
+
+-- 40
+SELECT 商品名, LENGTH(商品名) AS 文字数
+  FROM 商品
+ WHERE LENGTH(商品名) > 10
+ ORDER BY LENGTH(商品名)
+
+-- 41
+SELECT 注文日, SUBSTRING(注文番号, 9, 4) AS 注文番号
+  FROM 注文
+
+-- 42
+UPDATE 商品
+   SET 商品コード = 'E' || SUBSTRING(商品コード, 2, 4)
+ WHERE SUBSTRING(商品コード, 1, 1) = 'M'
+
+-- 43
+SELECT SUBSTRING(注文番号, 9, 4) AS 注文番号
+  FROM 注文
+ WHERE SUBSTRING(注文番号, 9, 1) IN ('1', '2')
+ ORDER BY SUBSTRING(注文番号, 9, 4)
+
+-- 44
+UPDATE 廃番商品
+   SET 廃番日 = CURRENT_DATE
+ WHERE 商品コード = 'S1990'
+
+-- 45
+SELECT TRUNC(単価*0.7, 0) AS 値下げした単価, 商品コード, 商品名, 単価 AS 現在の単価
+  FROM 商品
+ WHERE 単価 >= 10000
